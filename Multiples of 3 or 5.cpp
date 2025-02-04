9cb577b6627397127ad6802704c754748c4a8058
tubadeligoz <146365439+tubadeligoz@users.noreply.github.com>
2025-02-04 22:08:09 +0300

    Problem 1 : Multiples 3 or 5

---
+++ Multiples of 3 or 5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double total_sum = 0;
+
+	for (int number = 0;number < 1000;number++)
+	{
+		if (number % 3 == 0 || number % 5 == 0)
+		{
+			total_sum += number;
+		}
+	}
+	cout << "The sum of all multiples 3 or 5 below 1000 is" << total_sum << endl;
+	return 0;
+
+}
